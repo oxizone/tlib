bc6fa09dc95aada91b347530d06075055c9709f7
GURBULAK <bilal.gurbulak@edu.univ-fcomte.fr>
2022-11-16 12:02:13 +0100

    first commit

---
+++ src/main/java/com/ufr/tlib/TlibApplication.java
@@ -0,0 +1,13 @@
+package com.ufr.tlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufr/tlib/TlibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufr.tlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
